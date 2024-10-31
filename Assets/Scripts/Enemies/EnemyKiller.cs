ee9ae139a4095459c452a119af8eb5c9eba4b516
KalleRas <124579646+KalleRas@users.noreply.github.com>
2024-10-31 02:48:21 +0200

    Salmons dont kill each other

--- Assets/Scripts/Enemies/EnemyKiller.cs
+++ Assets/Scripts/Enemies/EnemyKiller.cs
@@ -6,10 +6,15 @@ public class EnemyKiller : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Salmon"))
+        // Check if the collider that entered has the "Player" tag
+        if (gameObject.CompareTag("Player"))
         {
-            var Salmon = other.gameObject.GetComponent<EnemyController>();
-            Salmon.Kill();
+            // Then check if the other GameObject has the "Salmon" tag
+            if (other.gameObject.CompareTag("Salmon"))
+            {
+                var salmon = other.gameObject.GetComponent<EnemyController>();
+                salmon.Kill();
+            }
         }
     }
 }
