8d887dd3f0c53537e7f80136f059f39e43bfb309
Eemil <c2toee01@students.oamk.fi>
2024-12-05 20:32:00 +0200

    Revert "Merge branch 'Oulu_Nights_Game' of https://github.com/eemilto/Oulu-Nights into Oulu_Nights_Game"
    This reverts commit 4a7382062fc927c00a9bd224beee14dc87f09a6a, reversing
    changes made to 86a0fdd42fe25dc1707b8bf576c4723e0dc58661.

--- Assets/Scripts/Enemies/EnemyBulletScript.cs
+++ Assets/Scripts/Enemies/EnemyBulletScript.cs
@@ -32,7 +32,7 @@ public class EnemyBulletScript : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if (timer > 5)
+        if (timer > 10)
         {
             Destroy(gameObject);
         }
