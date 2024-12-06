8e2399e56e20b59761017e65908ff00f59cfcb8a
KalleRas <124579646+KalleRas@users.noreply.github.com>
2024-12-07 01:29:14 +0200

    change

--- Assets/Scripts/Enemies/EnemyKiller.cs
+++ Assets/Scripts/Enemies/EnemyKiller.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class EnemyKiller : MonoBehaviour
 {
-    [SerializeField] private float damageAmount = 50f; // Damage dealt to the salmon
+    [SerializeField] private float damageAmount = 50f; // Damage dealt to the enemy
+    [SerializeField] private float bounceForce = 10f; // Upward force applied to the player
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,6 +13,20 @@ public class EnemyKiller : MonoBehaviour
             if (salmon != null)
             {
                 salmon.TakeDamage(damageAmount); // Apply damage
+            }
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody2D playerRb = other.GetComponent<Rigidbody2D>();
+            if (playerRb != null)
+            {
+                // Check if the player is coming from above
+                if (other.transform.position.y > transform.position.y)
+                {
+                    // Apply upward bounce force
+                    playerRb.velocity = new Vector2(playerRb.velocity.x, bounceForce);
+                }
             }
         }
     }
